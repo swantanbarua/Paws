beeb35d6a729e7d62ea7be322ed056428122b32e
Swantan Barua <work.swantan@gmail.com>
2025-09-14 12:54:46 +0530

    Add CustomContentUnavailableView SwiftUI component
    Introduces a new SwiftUI view named CustomContentUnavailableView with a placeholder text. This component can be used to display a message when content is unavailable.

---
+++ Paws/CustomContentUnavailableView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomContentUnavailableView.swift
+//  Paws
+//
+//  Created by Swantan Barua on 14/09/25.
+//
+
+import SwiftUI
+
+struct CustomContentUnavailableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomContentUnavailableView()
+}
