70302a67442413fd70f13902c3e8611a20291ddf
Swantan Barua <work.swantan@gmail.com>
2025-09-14 12:42:59 +0530

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Swantan Barua on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawsTests/PawsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PawsTests.swift
+//  PawsTests
+//
+//  Created by Swantan Barua on 14/09/25.
+//
+
+import Testing
+@testable import Paws
+
+struct PawsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
