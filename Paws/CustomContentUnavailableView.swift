800256d5530ac8c245a2914647930623d3dd9a8e
Swantan Barua <work.swantan@gmail.com>
2025-09-14 18:18:19 +0530

    Add properties to CustomContentUnavailableView
    Introduced icon, title, and description properties to CustomContentUnavailableView for improved customization. Updated the preview to use the new properties.

--- Paws/CustomContentUnavailableView.swift
+++ Paws/CustomContentUnavailableView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct CustomContentUnavailableView: View {
+    
+    // MARK: - PROPERTIES
+    var icon: String
+    var title: String
+    var description: String
+    
+    // MARK: - BODY
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CustomContentUnavailableView()
+    CustomContentUnavailableView(
+        icon: "cat.circle",
+        title: "No Photo",
+        description: "Add a photo to get started"
+    )
 }
