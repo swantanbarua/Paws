f7136228609b3c317b6a182ba7c17aefd89ad550
Swantan Barua <work.swantan@gmail.com>
2026-01-26 12:53:55 +0530

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Swantan Barua on 26/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
